eb2ad0bc00753a33c6893f1db3d0e572a49aa0b8
Rodrigo Medeiros <rodrig18@gmail.com>
2022-04-16 18:31:08 -0300

    Ajustes arquivo R e base sobre estacoes csv

--- R/wscemaden.R
+++ R/wscemaden.R
@@ -5,16 +5,18 @@
 #DATA: 10/07/2020                                                                                                                  #
 ####################################################################################################################################
 
+#Bibliotecas
 library(rvest)
 library(hms)
 library(lubridate)
 library(xlsx)
-library(readr)
 library(stringr)
 library(dplyr)
 
 #Obtem dados da pagina do Cemaden referente a cada estacao
+#id_station -> codigo identificador de uma estacao instalada em um municipio
 get.data.station <- function(id_station){
+  #Pagina fonte dos dados das estacoes
   url <- paste0('http://sjc.salvar.cemaden.gov.br/resources/graficos/interativo/consulta_dados.php?idpcd=', id_station)
   page <- read_html(url)
   table_html <- page %>%
@@ -24,13 +26,17 @@ get.data.station <- function(id_station){
 }
 
 #Obtem dados sobre cada estacao
+#loc -> Parametro para escolher um ou todos os municipios monitorados
 get.info.stations <- function(loc = 'all'){
+  #Arquivo com dados dos municipios incluindo dados espaciais
   fileName <- 'data/dados.estacoes.pa.csv'
-  data <- read_csv(fileName, 
-                   col_types = 'ccccccc')
+  data <- read.csv(fileName, 
+                   sep = ";")
+  #Validacao 
   if(!str_to_lower(loc) %in% str_to_lower(data$Municipio) & str_to_lower(loc) != 'all'){
     stop('Municipio nao encontrado na base!')
   }
+  #Dados para um determinado municipio
   if(str_to_lower(loc) != 'all'){
     data <- data %>%
      filter(str_to_lower(data$Municipio) %in% str_to_lower(loc))
@@ -44,21 +50,31 @@ save.pluv.data <- function(to_save){
   if(file.exists(fileName)){
     file.remove(fileName) 
   }
-  write.xlsx(to_save$data_st, file = fileName, sheetName = 'Coleta', append = TRUE)
-  write.xlsx(to_save$info_st, file = fileName, sheetName = 'Estacoes', append = TRUE)
+  write.xlsx(to_save$data_st, 
+             file = fileName, 
+             sheetName = 'Coleta', 
+             append = TRUE)
+  write.xlsx(to_save$info_st, 
+             file = fileName, 
+             sheetName = 'Estacoes', 
+             append = TRUE)
 }
 
 #Processa e retorna os dados obtidos da pagina do Cemaden
+#location -> Parametro para escolher um ou todos os municipios monitorados
 cemaden.pluv.data <- function(location = 'all'){
   data_df <- data.frame()
   ifelse(str_to_lower(location) != 'all', 
-         info_stations_df <- get.info.stations(location),
-         info_stations_df <- get.info.stations())
+         info_stations_df <- get.info.stations(location), #municipio especifico
+         info_stations_df <- get.info.stations()) # todos os municipios
   for(i in 1:nrow(info_stations_df)){
+    
     dstation_df <- get.data.station(info_stations_df$IdEstacao[i])
     info_stations_df[i, 'EstadoEstacao'] <- 'Ativada' 
+    
     #Flag de controle. Estacao sem coleta gera campo de data/hora vazio e que influencia no processamento 
     is_time_var_empty <- FALSE 
+    
     #Detecta estacao que possivelmente esteja desabilitada
     if(nrow(dstation_df) < 3){
       dstation_df$Chuva..mm. <- '0,00'
@@ -66,35 +82,50 @@ cemaden.pluv.data <- function(location = 'all'){
       info_stations_df[i, 'EstadoEstacao'] <- 'Desativada' 
       is_time_var_empty <- TRUE
     }
+    
     dstation_df$Id <- i
     info_stations_df[i, 'Id'] <- i
+    
     if(is_time_var_empty == FALSE) {
       dstation_df <- dstation_df[dstation_df$Data...Hora != 'Total', ]
     }
     #Tratamento do fuso-horario, pois os dados brutos estao com +3 horas em relacao ao horario local
     if(is_time_var_empty == FALSE){
-      dstation_df$Data...Hora <- dmy_hms(dstation_df$Data...Hora, tz='UTC')
-      dstation_df$Data...Hora <- with_tz(dstation_df$Data...Hora, 'America/Belem')
+      dstation_df$Data...Hora <- dmy_hms(dstation_df$Data...Hora, 
+                                         tz='UTC')
+      dstation_df$Data...Hora <- with_tz(dstation_df$Data...Hora, 
+                                         'America/Belem')
     }
-    dstation_df$DataColeta <- format(as.POSIXct(strptime(dstation_df$Data...Hora, '%Y-%m-%d %H:%M:%S', tz='')), format = '%d/%m/%Y')
-    dstation_df$HoraColeta <- format(as.POSIXct(strptime(dstation_df$Data...Hora, '%Y-%m-%d %H:%M:%S', tz='')), format = '%H:%M:%S')
+    
+    dstation_df$DataColeta <- format(as.POSIXct(strptime(dstation_df$Data...Hora, 
+                                                         '%Y-%m-%d %H:%M:%S', 
+                                                         tz='')), 
+                                     format = '%d/%m/%Y')
+    dstation_df$HoraColeta <- format(as.POSIXct(strptime(dstation_df$Data...Hora, 
+                                                         '%Y-%m-%d %H:%M:%S', 
+                                                         tz='')), 
+                                     format = '%H:%M:%S')
     dstation_df$Data...Hora <- NULL
     dstation_df <- dstation_df[complete.cases(dstation_df), ]
     dstation_df$IndHora <- 1:length(dstation_df$HoraColeta)
     data_df <- as.data.frame(rbind(data_df, dstation_df))
+    
+    #Exibicao em console para depuracao
     cat('Municipio: ', info_stations_df$Municipio[i],
         ' Estacao: ', info_stations_df$NomeEstacao[i],
         ' - ', info_stations_df$IdEstacao[i], '\n') 
     Sys.sleep(3) 
   }
-  return(list(data_st = data_df, info_st = info_stations_df))
+  return(list(data_st = data_df, 
+              info_st = info_stations_df))
 }
 
-#Teste
+#Execucao para as estacoes de todos os municipios (loc = 'all')
 data <- cemaden.pluv.data() 
+
 View(data$data_st)
 View(data$info_st)
-#save.pluv.data(data)
+
 
 
   
